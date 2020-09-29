9d00287b7c8159f8da199d43ff1c15d22002d10c
Oscar <oscarrhernandezoh@gmail.com>
2020-09-28 19:42:05 -0500

    Checkpoint C complete
    Health, health slider, Enemy AI

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth { get; private set; } = 100;
+    public int currentHealth { get; private set; } = 100;
+
+    //[SerializeField] AudioSource PlayerDamaged = null;
+
+    public event Action<int> HealthChanged = delegate { };
+
+    public void TakeDamage(int damage)
+    {
+       // PlayerDamaged.Play();
+        currentHealth -= damage;
+        HealthChanged.Invoke(currentHealth);
+        if (currentHealth <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "HealthPickup")
+        {
+            if (currentHealth < maxHealth)
+            {
+                currentHealth = maxHealth;
+                HealthChanged.Invoke(currentHealth);
+                other.gameObject.SetActive(false);
+            }
+        }
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G)){
+            TakeDamage(20);
+            Debug.Log("Current Health: " + currentHealth);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelHazard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelHazard : MonoBehaviour
+{
+    [SerializeField] Health _player = null;
+    //[SerializeField] AudioSource death = null;
+    //[SerializeField] ParticleSystem particles = null;
+
+    public Rigidbody enemy;
+
+    public float speed = 5f;
+    public float rotateSpeed = 200f;
+
+    public GameObject player;
+
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _player.TakeDamage(20);
+            //particles.Play();
+            //death.Play();
+            Invoke("Die", .05f);
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
